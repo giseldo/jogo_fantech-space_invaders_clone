6f99cb3ee6d26552b0dfd9bdaa1d5af9a07bff49
Kleber Galvao Filho <115438387+K-Galvao-Filho@users.noreply.github.com>
2025-03-18 08:37:43 -0300

    Tamanho da tela
    Devido ao tamanho de telas disponíveis... limitamos em 800x600.
    Ações futuras: Pretende-se que ele mantenha o padrão de tamanho 4:3 independente da tela.

--- js/game.js
+++ js/game.js
@@ -3,14 +3,15 @@ import { createBullets, updateBullets } from "./bullets.js"; // Importa funçõe
 
 const config = { // Configurações do jogo
     type: Phaser.AUTO, // Usa WebGL se disponível, senão usa Canvas
-    width: 1024, // Usa toda a largura disponível
-    height:768, // Usa toda a altura disponível
+    width: 800, // Usa toda a largura disponível
+    height:600, // Usa toda a altura disponível
     parent: 'game-container',
     physics: {
         default: 'arcade', // Motor de física Arcade
         arcade: { debug: false } // Desativa o modo de depuração
     },
     scene: { preload, create, update } // Funções de callback
+    
 };
 
 const game = new Phaser.Game(config); // Cria o jogo
